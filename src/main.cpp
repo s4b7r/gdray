834b56ddef01a5604270ece9d40dcd2e284253c9
sb <devnull@localhost>
2014-08-16 19:50:26 +0200

    Add help text

--- src/main.cpp
+++ src/main.cpp
@@ -29,14 +29,31 @@ void standard( char *modelfile, char *outputfile ) {
 
 int main(int argc, char **argv) {
 
-	if( argc == 2 && strcmp(argv[1], "--help") ) {
+	if( argc == 2 && !strcmp(argv[1], "--help") ) {
 		// Read this for more details...
-		// todo print help
+		printf("\n");
+		printf("GD_Ray\n");
+		printf("Simple raytracer by Simon Bekemeier and Kevin ***REMOVED***,\n");
+		printf("developed within computer graphics II course 2014\n");
+		printf("lectured by Prof. Dr. math. ***REMOVED*** ***REMOVED***\n");
+		printf("at Bielefeld University of Applied Sciences - \n");
+		printf("Department of Engineering Sciences and Mathematics.\n");
+		printf("\n");
+		printf("Usage:\n");
+		printf("GD_Ray MODELFILE OUTPUTFILE\n");
+		printf("Standard usage. Will load modelfile given by MODELFILE and save\n");
+		printf("raytracing result into OUTPUTFILE.\n");
+		printf("\n");
+		printf("GD_Ray --run-test\n");
+		printf("Run the developer's test procedure.\n");
+		printf("\n");
+		printf("GD_Ray --help\n");
+		printf("Show this help.\n");
+		printf("\n");
 	} else if( argc == 3 ) {
 		// Run the standard procedure
 		standard(argv[1], argv[2]);
-	}
-	else if( (argc == 2 && strcmp(argv[1], "--run-test")) || DEV_TEST ) {
+	} else if( (argc == 2 && !strcmp(argv[1], "--run-test")) || DEV_TEST ) {
 		// Run developer's test
 		test_start();
 	} else {
