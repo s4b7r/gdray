fda7c551c18a0b07ef0a0c35938677b02193f6bd
sb <devnull@localhost>
2014-08-22 15:23:42 +0200

    Start documentation

--- src/math_noc.h
+++ src/math_noc.h
@@ -8,7 +8,7 @@
 #ifndef MATH_NOC_H_
 #define MATH_NOC_H_
 
-// Use mathematic knowledge to understand struct ray
+// Use mathematical knowledge to understand struct ray ;)
 struct ray {
 
 	Vector3d point;
